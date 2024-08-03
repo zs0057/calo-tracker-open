b3e04f73597d99f63cebe007cb67aefeb046258d
zs0057 <soss4823@gmail.com>
2024-08-03 11:45:43 +0900

    f cors

--- app/api/estimateCal/route.ts
+++ app/api/estimateCal/route.ts
@@ -2,6 +2,18 @@ import { openAiRequest } from "@/lib/api/openapi";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  const response = NextResponse.next();
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
   try {
     const body = await req.json(); // JSON 본문을 파싱
     const text = body.description;
